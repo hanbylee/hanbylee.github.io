7a68debf6b55e4b3669c74bf917a50dc27ff9016
hanby-uxengineer <yihaanstar@gmail.com>
2022-07-07 16:20:15 +0900

    custom domain test

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hanbyeol's Portfolio Website
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hanby-uxengineer.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hanby-uxengineer.github.io
         </a>
       </header>
     </div>
