c395214bd1bd2026ba019bc8441fe96423da1a60
hanby-uxengineer <yihaanstar@gmail.com>
2022-07-10 23:11:16 +0900

    add scss files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles/css/test.css';
+import './App.scss';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hanbyeol's Portfolio Website
-        </p>
-        <a
-          className="App-link"
-          href="https://hanby-uxengineer.github.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hanby-uxengineer.github.io
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
